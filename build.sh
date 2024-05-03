d32b8d60b566bc9efc66d339ddb013046792c2a1
Alex Ivanov <alexivanov@Alexs-MBP-2.ht.home>
2024-05-02 22:16:39 -0400

    built on mac

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 python setup.py bdist_wheel
 
 # install to site-packages
-pip install dist/tokenizers_cpp-0.0.1-cp310-cp310-linux_x86_64.whl --force-reinstall
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Installing on Mac"
+    pip install dist/tokenizers_cpp-0.0.1-cp310-cp310-macosx_11_0_arm64.whl --force-reinstall
+else
+    # assume line
+    echo "Assuming Linux. Installing on Linux"
+    pip install dist/tokenizers_cpp-0.0.1-cp310-cp310-linux_x86_64.whl --force-reinstall
+fi
+
